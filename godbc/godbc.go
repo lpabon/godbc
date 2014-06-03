1bb1a18a817667ddf912c44cb8cceb76c552ed9d
Luis Pabon <lpabon@redhat.com>
2014-06-02 21:47:36 -0400

    Travis support and cleaned up some of the code

--- godbc/godbc.go
+++ godbc/godbc.go
@@ -1,34 +1,42 @@
 package godbc
 
 import (
+	"errors"
 	"fmt"
 	"runtime"
 )
 
-func Require(b bool, message ...interface{}) {
+func dbc_panic(dbc_func_name string, b bool, message ...interface{}) {
 	if !b {
-		pc, file, line, ok := runtime.Caller(1)
-		finfo := runtime.FuncForPC(pc)
-		fmt.Println("Hello")
-		fmt.Printf("Func %s - %s - %d - %t",
-			finfo.Name(),
+
+		// Get caller information which is the caller
+		// of the caller of this function
+		pc, file, line, _ := runtime.Caller(2)
+		caller_func_info := runtime.FuncForPC(pc)
+
+		error_string := fmt.Sprintf("%s: %s:%s:%d",
+			dbc_func_name,
 			file,
-			line,
-			ok)
-		panic("Failed---")
+			caller_func_info.Name(),
+			line)
+		if len(message) > 0 {
+			error_string += fmt.Sprintf("\n\rMessage:%+v len %d", message, len(message))
+		}
+		err := errors.New(error_string)
+
+		// Finally panic
+		panic(err)
 	}
 }
 
-func Ensure(b bool) {
-	if !b {
-		panic("Ensure context failed")
-	}
+func Require(b bool, message ...interface{}) {
+	dbc_panic("REQUIRE", b, message...)
 }
 
-func Check() {
-	fmt.Println("In Check")
+func Ensure(b bool, message ...interface{}) {
+	dbc_panic("ENSURE", b, message...)
 }
 
-func Invariant() {
-	fmt.Println("In Inv")
+func Check(b bool, message ...interface{}) {
+	dbc_panic("CHECK", b, message...)
 }
--- godbc/godbc_test.go
+++ godbc/godbc_test.go
@@ -1,24 +1,51 @@
 package godbc
 
 import (
-	//	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRequire(t *testing.T) {
-	a := 0
-	Require(a != 0)
-	/*
-		assert.Panics(t, func() {
-			Require(a == b)
-		})
+	a, b := 0, 1
+	assert.Panics(t, func() {
+		Require(a == b)
+	})
 
-		assert.NotPanics(t, func() {
-			Require(a != b)
-		})
+	assert.NotPanics(t, func() {
+		Require(a != b)
+	})
 
-		assert.Panics(t, func() {
-			Require(a > 0)
-		})
-	*/
+	assert.Panics(t, func() {
+		Require(a > 0)
+	})
+}
+
+func TestEnsure(t *testing.T) {
+	a, b := 0, 1
+	assert.Panics(t, func() {
+		Ensure(a == b)
+	})
+
+	assert.NotPanics(t, func() {
+		Ensure(a != b)
+	})
+
+	assert.Panics(t, func() {
+		Ensure(a > 0)
+	})
+}
+
+func TestCheck(t *testing.T) {
+	a, b := 0, 1
+	assert.Panics(t, func() {
+		Check(a == b)
+	})
+
+	assert.NotPanics(t, func() {
+		Check(a != b)
+	})
+
+	assert.Panics(t, func() {
+		Check(a > 0)
+	})
 }
