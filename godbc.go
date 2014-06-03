b19a48a49fa24b545db02c024fca17122843aa22
Luis Pabon <lpabon@redhat.com>
2014-06-03 16:47:09 -0400

    Added Godoc documentation

--- godbc.go
+++ godbc.go
@@ -1,18 +1,33 @@
-/*
- * Copyright 2014 Luis Pabon, Jr.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+// Copyright 2014 Luis Pabon, Jr.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Design-by-Contract for Go
+//
+// Design by Contract is a programming methodology
+// which binds the caller and the function called to a
+// contract. The contract is represented using Hoare Triple:
+//      {P} C {Q}
+// where {P} is the precondition before executing command C,
+// and {Q} is the postcondition.
+//
+// See Also
+//
+// * http://en.wikipedia.org/wiki/Design_by_contract
+// * http://en.wikipedia.org/wiki/Hoare_logic
+// * http://dlang.org/dbc.html
+//
 package godbc
 
 import (
@@ -21,15 +36,21 @@ import (
 	"runtime"
 )
 
+// InvariantSimpleTester is an interface which provides a receiver to
+// test the object
 type InvariantSimpleTester interface {
 	Invariant() bool
 }
 
+// InvariantTester is an interface which provides not only an Invariant(),
+// but also a receiver to print the structure
 type InvariantTester interface {
 	InvariantSimpleTester
 	String() string
 }
 
+// dbc_panic prints to the screen information of the failure followed
+// by a call to panic()
 func dbc_panic(dbc_func_name string, b bool, message ...interface{}) {
 	if !b {
 
@@ -55,22 +76,58 @@ func dbc_panic(dbc_func_name string, b bool, message ...interface{}) {
 	}
 }
 
+// Require checks that the preconditions are satisfied before
+// executing the function
+//
+// Example
+//
+// 		func Divide(a, b int) int {
+//			godbc.Require(b != 0)
+//			return a/b
+// 		}
+//
 func Require(b bool, message ...interface{}) {
 	dbc_panic("REQUIRE", b, message...)
 }
 
+// Ensure checks the postconditions are satisfied before returning
+// to the caller.
+//
+// Example
+//
+//		type Data struct {
+//			a int
+//		}
+//
+// 		func (*d Data) Set(a int) {
+//			d.a
+//			godbc.Ensure(d.a == a)
+// 		}
+//
 func Ensure(b bool, message ...interface{}) {
 	dbc_panic("ENSURE", b, message...)
 }
 
+// Check provides a simple assert
 func Check(b bool, message ...interface{}) {
 	dbc_panic("CHECK", b, message...)
 }
 
+// InvariantSimple calls the objects Invariant() receiver to test
+// the object for correctness.
+//
+// The caller object must provide an object that supports the
+// interface InvariantSimpleTester and does not need to provide
+// a String() receiver
 func InvariantSimple(obj InvariantSimpleTester, message ...interface{}) {
 	dbc_panic("INVARIANT", obj.Invariant(), message...)
 }
 
+// Invariant calls the objects Invariant() receiver to test
+// the object for correctness.
+//
+// The caller object must provide an object that supports the
+// interface InvariantTester
 func Invariant(obj InvariantTester, message ...interface{}) {
 	m := append(message, obj)
 	dbc_panic("INVARIANT", obj.Invariant(), m)
