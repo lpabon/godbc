9577782540c1398b710ddae1b86268ba03a19b0c
Luis Pabon <lpabon@redhat.com>
2014-06-13 12:58:03 -0400

    Changed copyrights to Google Chromium style

--- godbc.go
+++ godbc.go
@@ -1,7 +1,7 @@
 //+build !prod
 
 //
-// Copyright 2014 Luis Pabon, Jr.
+// Copyright (c) 2014 The godbc Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
